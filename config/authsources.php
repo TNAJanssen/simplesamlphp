88a2e214622647015f5b2ace2e58312e8a2adf50
Alexander Janssen <alexander@dutchbridge.nl>
2013-11-29 12:46:04 +0100

    update cert location;

--- config/authsources.php
+++ config/authsources.php
@@ -2,6 +2,6 @@
 
 $config = array(
     'entityid' => 'https://was-preprod1.digid.nl/saml/idp/metadata',
-    'privatekey' => '/Users/AJanssen/Projecten/geotax/backend/app/cert/saml.pem',
-    'certificate' => '/Users/AJanssen/Projecten/geotax/backend/app/cert/saml.crt',
+    'privatekey' => __DIR__.'/../../../../app/cert/saml.pem',
+    'certificate' => __DIR__.'/../../../../app/cert/saml.crt',
 );
